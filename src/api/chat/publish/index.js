c6b2e138f9f60d1b8d854390a5038cb317dc95a3
amirhnajafiz <najafizadeh21@gmail.com>
2022-03-12 08:52:25 +0330

    api updated

---
+++ src/api/chat/publish/index.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+let URL = "";
+
+function publish(username, jwt) {
+    URL = URL + '?' + 'username=' + username + '&' + 'jwt=' + jwt;
+
+    let data = undefined;
+
+    axios.get(URL)
+        .then(response => {
+            console.log("OK")
+            data = response;
+        })
+        .catch(err => {
+            console.error(err);
+        })
+    
+    return data
+}
+
+export default publish
