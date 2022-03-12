43c78eaa99d496ca777bc7d8a8e00114d16af3aa
amirhnajafiz <najafizadeh21@gmail.com>
2022-03-12 16:03:58 +0330

    nats successfully added

--- src/main.js
+++ src/main.js
@@ -9,15 +9,16 @@ import vueNats from 'vue-nats';
 const app = createApp(App)
 app.use(router)
 app.use(store)
-app.use(vueNats, {
-    url: 'ws://<nats-wesocket-relay>:8080',
-    json: true, // use JSON data payload
-    reconnect: true, // always reconnect
-    maxReconnectAttempts: -1, // retry forever
-    reconnectTimeWait: 1000 // try to reconnect every second
-});
 
 axios.defaults.headers.common['Content-Type'] ='application/x-www-form-urlencoded';
 axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 
 app.mount('#app')
+
+app.use(vueNats, {
+    url: 'ws://<nats-wesocket-relay>:8080',
+    json: true, // use JSON data payload
+    reconnect: false, // always reconnect
+    maxReconnectAttempts: -1, // retry forever
+    reconnectTimeWait: -1 // try to reconnect every second
+});
