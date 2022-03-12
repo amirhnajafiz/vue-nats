1a2b956a9f2952afc472e48f579c6b329eaf4411
amirhnajafiz <najafizadeh21@gmail.com>
2022-03-12 11:56:26 +0330

    chats view in room added

--- src/components/Room.vue
+++ src/components/Room.vue
@@ -1,12 +1,22 @@
 <template>
   <div>
-      <div>
-          <div v-for="chat in chats" :key="chat.id">
+      <div class="main-panel">
+          <div v-for="chat in chats" :key="chat.id" class="row">
+              <div class="full-flex">
+                  <div class="circle">
+                      <span>
+                          {{ chat.sender.split(' ').slice(0, 2).join(' ').toUpperCase() }}
+                      </span>
+                  </div>
+                  <small>
+                      {{ chat.sender }}
+                  </small>
+              </div>
               <p>
                   {{ chat.message }}
               </p> 
-              <small>
-                  {{ chat.sender + " | " + chat.date }}
+              <small class="small-text">
+                  {{ chat.date }}
               </small>
           </div>
       </div>
@@ -24,6 +34,36 @@ export default {
                     sender: "me",
                     message: "hi",
                     date: new Date(),
+                },
+                {
+                    id: "2",
+                    sender: "me",
+                    message: "hi",
+                    date: new Date(),
+                },
+                {
+                    id: "3",
+                    sender: "me",
+                    message: "hi",
+                    date: new Date(),
+                },
+                {
+                    id: "4",
+                    sender: "me",
+                    message: "hi",
+                    date: new Date(),
+                },
+                {
+                    id: "5",
+                    sender: "me",
+                    message: "hi",
+                    date: new Date(),
+                },
+                {
+                    id: "6",
+                    sender: "me",
+                    message: "hi",
+                    date: new Date(),
                 }
             ]
         }
@@ -31,6 +71,47 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.main-panel {
+    background-color: #fff;
+    padding: 10px;
+    border-radius: 5px;
+    width: 80%;
+    margin: 10px auto;
+    height: 600px;
+    overflow: scroll;
+}
 
+.row {
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    text-align: left;
+    border-bottom: 1px solid gainsboro;
+}
+
+.circle {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 30px;
+    height: 30px;
+    background: rgb(0, 64, 99);
+    color: #fff;
+    border-radius: 50%;
+    margin-right: 10px;
+    font-size: 12px;
+}
+
+.full-flex {
+    display: flex;
+    flex-direction: row;
+    align-content: center;
+    align-items: center;
+}
+
+.small-text {
+    font-size: 0.5em;
+}
 </style>
