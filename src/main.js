e46960df474d2158241fbb1198690a0e2865c715
amirhnajafiz <najafizadeh21@gmail.com>
2022-03-12 09:50:15 +0330

    router init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import axios from "axios";
+import axios from "axios"
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes: [],
+})
+
+const app = createApp(App)
+app.use(router)
 
 axios.defaults.headers.common['Content-Type'] ='application/x-www-form-urlencoded';
 axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 
-createApp(App).mount('#app')
+app.mount('#app')
