536c0e0dfe0d6eff5a3ff4df0a850d68e2f52c87
amirhnajafiz <najafizadeh21@gmail.com>
2022-03-13 18:38:49 +0330

    main updated and refactored

--- src/main.js
+++ src/main.js
@@ -1,20 +1,27 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import axios from "axios"
+import axios from 'axios'
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import router from './router'
 import store from './store'
 import vueNats from 'vue-nats';
 
-// TODO: refactor the main
+// creating a vue app
 const app = createApp(App)
+// router and store
 app.use(router)
 app.use(store)
+// vue plugins
+app.use(BootstrapVue)
+app.use(IconsPlugin)
+// app mounting
+app.mount('#app')
 
+// axios config
 axios.defaults.headers.common['Content-Type'] ='application/x-www-form-urlencoded';
 axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 
-app.mount('#app')
-
+// nats config
 app.use(vueNats, {
     url: process.env.NATS_HOST,
     json: true, // use JSON data payload
