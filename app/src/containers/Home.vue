8859da4dd5c1804ac63d18fddb11df698ae82139
amirhnajafiz <najafizadeh21@gmail.com>
2023-03-25 17:00:54 +0330

    update: home

--- app/src/containers/Home.vue
+++ app/src/containers/Home.vue
@@ -1,12 +1,15 @@
 <template>
   <h1>
-    Hello Home
+    Vue-Nats
   </h1>
+  <EventBox />
 </template>
 
 <script>
+import EventBox from "../components/EventBox.vue";
 export default {
-  name: "Home"
+  name: "Home",
+  components: {EventBox}
 }
 </script>
 
