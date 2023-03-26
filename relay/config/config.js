76e2f27e57571eaeaeb99127ce73cf03cb496b29
amirhnajafiz <najafizadeh21@gmail.com>
2023-03-26 14:53:39 +0330

    add: topic in configs

--- relay/config/config.js
+++ relay/config/config.js
@@ -9,7 +9,8 @@ class Config {
     constructor() {
         this.port = process.env.PORT;
         this.nats = `${process.env.NATS_PROTOCOL}://${process.env.NATS_CLUSTER}:${process.env.NATS_PORT}`;
-        this.timeout = process.env.TIMEOUT
+        this.timeout = process.env.TIMEOUT;
+        this.topic = process.env.TOPIC;
     }
 }
 
