a42802bc418ea58bb4fa8dd11a6c0fcbe88f7102
amirhnajafiz <najafizadeh21@gmail.com>
2023-03-26 14:51:44 +0330

    update: events

--- relay/model/event.js
+++ relay/model/event.js
@@ -7,19 +7,16 @@ class Event {
 
     setPayload(payload) {
         this.payload = payload;
-
         return this;
     }
 
     setTime(time) {
         this.time = time;
-
         return this;
     }
 
     setSender(sender) {
         this.sender = sender;
-
         return this;
     }
 }
