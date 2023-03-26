00e4b32b3f75dc30a5a66f60a431b7eba7ef22b3
amirhnajafiz <najafizadeh21@gmail.com>
2023-03-26 13:40:10 +0330

    init: config load

---
+++ relay/config/config.js
@@ -0,0 +1,12 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+class Config {
+    constructor() {
+        this.port = process.env.PORT;
+        this.nats = `${process.env.NATS_PROTOCOL}://${process.env.NATS_CLUSTER}:${process.env.NATS_PORT}`;
+        this.timeout = process.env.TIMEOUT
+    }
+}
+
+export default Config;
