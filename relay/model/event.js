27287dba1a5ca11746fba353465ea1277df69616
amirhnajafiz <najafizadeh21@gmail.com>
2023-03-26 13:47:22 +0330

    add: event module

---
+++ relay/model/event.js
@@ -0,0 +1,25 @@
+class Event {
+    constructor() {
+        this.payload = "";
+        this.time = new Date();
+        this.sender = "";
+    }
+
+    setPayload(payload) {
+        this.payload = payload;
+
+        return this;
+    }
+
+    setTime(time) {
+        this.time = time;
+
+        return this;
+    }
+
+    setSender(sender) {
+        this.sender = sender;
+
+        return this;
+    }
+}
