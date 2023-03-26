085e1babfadd608ba9c3b96cbf85d4ca602be5d7
amirhnajafiz <najafizadeh21@gmail.com>
2023-03-26 15:09:21 +0330

    add: websocket

--- relay/index.js
+++ relay/index.js
@@ -6,7 +6,6 @@ const config = require('./config/config');
 const express = require('express');
 const http = require('http');
 const ws = require('ws');
-const e = require("express");
 
 
 // main function
@@ -24,6 +23,26 @@ async function main() {
     if (nc === null) {
         console.error("nats connection failed");
     }
+
+    // create express app
+    const app = express();
+    // initialize a simple http server
+    const server = http.createServer(app);
+    // initialize the WebSocket server instance
+    const wss = new WebSocket.Server({ server });
+
+    wss.on('connection', (ws) => {
+        ws.on('message', (message) => {
+            console.log('received: %s', message);
+            ws.send(`Hello, you sent -> ${message}`);
+        });
+        ws.send('Hi there, I am a WebSocket server');
+    });
+
+    // start our server
+    server.listen(cfg.port || 8999, () => {
+        console.log(`Server started on port ${server.address().port} :)`);
+    });
 }
 
 // start index
