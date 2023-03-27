0e49790111ef1ff37f8244e2e00cd40f683fa660
amirhnajafiz <najafizadeh21@gmail.com>
2023-03-27 12:46:33 +0330

    fix: color styles

--- app/src/containers/Home.vue
+++ app/src/containers/Home.vue
@@ -5,7 +5,7 @@ import EventBox from "../components/EventBox.vue";
 <template>
   <div class="container">
     <h1 class="title">
-      Vue-Nats
+      Vue Nats
     </h1>
     <EventBox
         class="box"
@@ -21,17 +21,19 @@ export default {
 
 <style scoped>
 .title {
-  margin: 50px auto;
+  margin: 100px auto 0;
   text-align: center;
 }
 
 .box {
   width: 50%;
-  margin: 100px auto 0;
+  margin: 70px auto 0;
   padding: 20px 40px;
-  border: 2px solid gainsboro;
+  border: 3px solid rgba(14, 47, 42, 0.92);
   border-radius: 5px;
   height: 350px;
   overflow: scroll;
+  background-color: #163260;
+  box-shadow: 0 0 15px rgba(9, 9, 9, 0.5);
 }
 </style>
